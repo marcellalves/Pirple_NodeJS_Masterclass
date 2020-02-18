9734e983e034d509bb2e95a33fbc7d210e3de64c
Marcell Alves <marcell.alves@gmail.com>
2020-02-18 15:45:48 -0300

    Adicionando CRUD de usuário

--- Building a RESTful API/Example/index.js
+++ Building a RESTful API/Example/index.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
+var config = require('./lib/config');
+var handlers = require('./lib/handlers');
+var helpers = require('./lib/helpers');
 
 var httpServer = http.createServer(function(req, res) {
     unifiedServer(req, res);
@@ -40,7 +42,7 @@ var unifiedServer = function(req, res) {
             'queryStringObject': queryStringObject,
             'method': method,
             'headers': headers,
-            'payload': buffer
+            'payload': helpers.parseJsonToObject(buffer)
         };
 
         // Route the request to the handler specified in the router
@@ -60,16 +62,7 @@ var unifiedServer = function(req, res) {
     });
 }
 
-var handlers = {};
-
-handlers.ping = function(data, callback) {
-    callback(200);
-}
-
-handlers.notFound = function(data, callback) {
-    callback(404);
-}
-
 var router = { 
-    'ping' : handlers.ping
+    'ping' : handlers.ping,
+    'users': handlers.users
 }
